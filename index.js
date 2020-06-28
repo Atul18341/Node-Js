74be9914c5003cc2668ae88f0f2b633efe2e37cf
Atul Kumar <atulkumar987613@gmail.com>
2020-06-28 15:20:25 +0530

    Updated file with Callback Function & Error Handling

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var rect=require('./rectangle');
 
 function cal_rect(l,b){
 	console.log("Calculating for rectangle with l="+l+" and b="+b);
-	if(l<=0 || b<=0){
-		console.log("Dimensions of rectangle must be greater than 0, l="+l+" and b="+b);
+	rect(l,b,(err,rectangle)=>{
+		if(err){
+		console.log("Error:",err.message);
+	    }
+	    else{
+        console.log("Perimeter of rectangle with l="+l+" and b="+b+ " is "+rectangle.perimeter());
+		console.log("Area of rectangle with l="+l+" and b="+b+" is "+rectangle.area());
+	    }
 
-	}
-	else{
-		console.log("Perimeter of rectangle with l="+l+" and b="+b+ " is "+rect.perimeter(l,b));
-		console.log("Area of rectangle with l="+l+" and b="+b+" is "+rect.area(l,b));
-	}
-}
+	});
+
+  console.log("This statement after call to rect()");
+};
 
 cal_rect(9,8);
 cal_rect(6,3);
